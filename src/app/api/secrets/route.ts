47656d95685d621701acfcaa5ec85ac94d591b94
alvaro <alvarodevcode@outlook.com>
2023-11-13 20:36:20 -0500

    feat: dropped access to PUT, DELETE secret API

--- src/app/api/secrets/route.ts
+++ src/app/api/secrets/route.ts
@@ -2,13 +2,9 @@ import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
-import { createSecret, deleteSecret, updateSecret } from "@/lib/api/secrets/mutations";
+import { createSecret } from "@/lib/api/secrets/mutations";
 import { getSecrets } from "@/lib/api/secrets/queries";
-import {
-  insertSecretParams,
-  secretIdSchema,
-  updateSecretParams,
-} from "@/lib/db/schema/secrets";
+import { insertSecretParams } from "@/lib/db/schema/secrets";
 
 /**
  *
@@ -45,46 +41,6 @@ export async function POST(req: Request) {
       return NextResponse.json({ error: err.issues }, { status: 400 });
     } else {
       return NextResponse.json({ error: err }, { status: 500 });
-    }
-  }
-}
-
-export async function PUT(req: Request) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const id = searchParams.get("id");
-
-    const validatedData = updateSecretParams.parse(await req.json());
-    const validatedParams = secretIdSchema.parse({ id });
-
-    const { success, error } = await updateSecret(validatedParams.id, validatedData);
-
-    if (error) return NextResponse.json({ error }, { status: 500 });
-    return NextResponse.json(success, { status: 200 });
-  } catch (err) {
-    if (err instanceof z.ZodError) {
-      return NextResponse.json({ error: err.issues }, { status: 400 });
-    } else {
-      return NextResponse.json(err, { status: 500 });
-    }
-  }
-}
-
-export async function DELETE(req: Request) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const id = searchParams.get("id");
-
-    const validatedParams = secretIdSchema.parse({ id });
-    const { success, error } = await deleteSecret(validatedParams.id);
-    if (error) return NextResponse.json({ error }, { status: 500 });
-
-    return NextResponse.json(success, { status: 200 });
-  } catch (err) {
-    if (err instanceof z.ZodError) {
-      return NextResponse.json({ error: err.issues }, { status: 400 });
-    } else {
-      return NextResponse.json(err, { status: 500 });
     }
   }
 }
