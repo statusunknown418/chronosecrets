5534d5260527d4b9249a72aac9ad9d7b526090ea
alvaro <alvarodevcode@outlook.com>
2023-11-21 12:01:01 -0500

    fix: fixes loading ui

--- src/app/(authenticated)/receiving/loading.tsx
+++ src/app/(authenticated)/receiving/loading.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Spinner } from "@/components/ui/spinner";
+import { Skeleton } from "@/components/ui/skeleton";
 import { X } from "lucide-react";
 import Link from "next/link";
 
@@ -9,7 +9,7 @@ export default function LoadingAllReceivingPage() {
       <section className="sticky inset-0 z-10 flex flex-col gap-2 border-b bg-background/20 px-4 py-2 backdrop-blur backdrop-filter sm:py-4">
         <header className="flex w-full items-center justify-between gap-4">
           <h1 className="text-2xl font-bold capitalize tracking-tight sm:text-3xl">
-            Sent to you
+            Sent to me
           </h1>
 
           <Link href="/home" className="focus-within:outline-none">
@@ -20,7 +20,11 @@ export default function LoadingAllReceivingPage() {
         </header>
       </section>
 
-      <Spinner />
+      <section className="grid grid-cols-1 gap-4 px-4 md:grid-cols-2">
+        {[1, 2, 3, 4, 5, 6].map((s) => (
+          <Skeleton key={s} className="h-64 w-full" />
+        ))}
+      </section>
     </div>
   );
 }
