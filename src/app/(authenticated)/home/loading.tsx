72a77158f4f991eeb09ea371ecbfd00c925c7573
alvaro <alvarodevcode@outlook.com>
2023-11-22 20:27:25 -0500

    feat: updated loading states

--- src/app/(authenticated)/home/loading.tsx
+++ src/app/(authenticated)/home/loading.tsx
@@ -1,5 +1,18 @@
-import { Spinner } from "@/components/ui/spinner";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function HomePageLoader() {
-  return <Spinner />;
+  return (
+    <section className="flex h-full flex-col gap-4">
+      <div className="flex items-center justify-start">
+        <Skeleton className="h-10 w-20 rounded-l-full" />
+        <Skeleton className="h-10 w-20 rounded-r-full" />
+      </div>
+
+      <section className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
+        {[1, 2, 3].map((s) => (
+          <Skeleton key={s} className="h-64 w-full" />
+        ))}
+      </section>
+    </section>
+  );
 }
--- src/app/(authenticated)/receiving/loading.tsx
+++ src/app/(authenticated)/receiving/loading.tsx
@@ -8,9 +8,7 @@ export default function LoadingAllReceivingPage() {
     <div className="flex h-full w-full flex-col gap-4">
       <section className="sticky inset-0 z-10 flex flex-col gap-2 border-b bg-background/20 px-4 py-2 backdrop-blur backdrop-filter sm:py-4">
         <header className="flex w-full items-center justify-between gap-4">
-          <h1 className="text-2xl font-bold capitalize tracking-tight sm:text-3xl">
-            Sent to me
-          </h1>
+          <h1 className="text-2xl font-bold tracking-tight sm:text-3xl">Sent to me</h1>
 
           <Link href="/home" className="focus-within:outline-none">
             <Button variant="ghost" size="icon">
@@ -21,7 +19,7 @@ export default function LoadingAllReceivingPage() {
       </section>
 
       <section className="grid grid-cols-1 gap-4 px-4 md:grid-cols-2">
-        {[1, 2, 3, 4, 5, 6].map((s) => (
+        {[1, 2, 3].map((s) => (
           <Skeleton key={s} className="h-64 w-full" />
         ))}
       </section>
